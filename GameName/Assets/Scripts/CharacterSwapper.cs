c86d3d385513a6e7a23dcb2041db5513b26c2cc3
peter <hguanqi7@gmail.com>
2021-07-28 12:04:12 +0800

    Revert "Revert "Character Controllers + Character Swapper""
    This reverts commit bc4a7a93156541760e80d60e2885fa5c6376dd6f.

---
+++ GameName/Assets/Scripts/CharacterSwapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwapper : MonoBehaviour
+{
+    public bool m_IsGiant;
+
+    public GameObject m_Boy;
+    public GameObject m_Giant;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            m_IsGiant = !m_IsGiant;
+        }
+
+        if(m_IsGiant)
+        {
+            m_Giant.GetComponent<CharacterMotor>().enabled = false;
+            m_Giant.GetComponent<CharacterMotor>().m_Look.m_AttachedCamera.enabled = false;
+            m_Boy.GetComponent<CharacterMotor>().enabled = true;
+            m_Boy.GetComponent<CharacterMotor>().m_Look.m_AttachedCamera.enabled = true;
+        }
+        else
+        {
+            m_Giant.GetComponent<CharacterMotor>().enabled = true;
+            m_Giant.GetComponent<CharacterMotor>().m_Look.m_AttachedCamera.enabled = true;
+            m_Boy.GetComponent<CharacterMotor>().enabled = false;
+            m_Boy.GetComponent<CharacterMotor>().m_Look.m_AttachedCamera.enabled = false;
+        }
+    }
+}
