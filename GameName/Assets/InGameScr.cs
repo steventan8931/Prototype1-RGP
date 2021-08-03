6423e7a75c9417ab556216fd69ed649721960697
peter <hguanqi7@gmail.com>
2021-08-03 20:35:08 +0800

    Added Game-Over UI and Win UI, recreated Boy And Giant UI

---
+++ GameName/Assets/InGameScr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameScr : MonoBehaviour
+{
+    public GameObject boy, giant;
+    public GameObject GameManager;//get the game manager object
+    public GameObject WinUI;
+    public Boy boyscr;
+    public GameState winstate;
+    public GameObject DetectedUi;
+
+    public void checkDetect()
+    {
+        if(boyscr.m_Detected)
+        {
+            DetectedUi.SetActive(true);
+        }else
+        {
+            DetectedUi.SetActive(false);
+        }
+    }
+
+    public void checkWin()
+    {
+        if(winstate.m_GameWin)
+        {
+            WinUI.SetActive(true);
+
+        }
+    }
+
+    public void Update()
+    {
+        checkDetect();
+        checkWin();
+    }
+}
