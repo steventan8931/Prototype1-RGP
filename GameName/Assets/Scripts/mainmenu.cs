c4ba8c3012e87c0b033e15676440ef0f5bdedde1
Steven Tan <tan2001steven@hotmail.com>
2021-08-03 12:46:41 +1200

    Auto stash before rebase of "origin/Steven2"

--- GameName/Assets/Scripts/mainmenu.cs
+++ GameName/Assets/Scripts/mainmenu.cs
@@ -6,23 +6,9 @@ public class mainmenu : MonoBehaviour
 {
     public GameObject Basiclayer;
     public GameObject Tutoriallayer;
-    public Animator transistion;
-    public float transistionTime = 1f;
-    //If you want to add a transistion animation to game scene, u need to make scene loader a sepereate prefab and drag it 
-    //into the game scene.
     public void startgame()
     {
-        transistion.SetTrigger("Start");
-        print("trigger activated");   
-        Invoke("loadscene", transistionTime);
-    }
-       
-        
-
-    public void loadscene()
-    {
         SceneManager.LoadScene(1);
-        
     }
 
     public void openTutorial()
