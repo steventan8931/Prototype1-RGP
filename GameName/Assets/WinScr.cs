01f2a0d50acae53424bdeb6a780d60fffce07a6e
peter <hguanqi7@gmail.com>
2021-08-04 18:39:04 +0800

    Win UI fixes

--- GameName/Assets/WinScr.cs
+++ GameName/Assets/WinScr.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinScr : MonoBehaviour
 {
     public GameObject WinUi;
     public GameObject Manager;
+    public GameObject Exitbutton, MenuButton;
 
-    
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+
+    public void loadmenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     void Update()
     {
         if(Manager.GetComponent<GameState>().m_GameWin)
