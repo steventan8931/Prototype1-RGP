29c2bf3b4ed93d6f53f96b837cf38daddc6c8815
Steven Tan <tan2001steven@hotmail.com>
2021-08-04 21:53:59 +1200

    Auto stash before merge of "Steven3" and "origin/UI5"

--- GameName/Assets/Scripts/Death.cs
+++ GameName/Assets/Scripts/Death.cs
@@ -6,11 +6,14 @@ public class Death : MonoBehaviour
 {
     public bool m_TouchedWater = false;
 
+    public AudioSource m_Audio;
+
     private void OnTriggerEnter(Collider _other)
     {
         if (_other.tag == "Boy")
         {
             _other.GetComponent<Boy>().m_Killed = true;
+            m_Audio.Play();
         }
     }
 }
