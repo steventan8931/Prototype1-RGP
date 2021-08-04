67ee358ccecb55fcf8198514b2cba1a1ff780962
peter <hguanqi7@gmail.com>
2021-08-04 17:49:30 +0800

    Burn UI added

---
+++ GameName/Assets/BurnUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnUI : MonoBehaviour
+{
+    public GameObject burnUI;
+    public GameObject manager;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(manager.GetComponent<GameState>().m_BoyDead)
+        {
+            burnUI.SetActive(true);
+        }else
+        {
+            burnUI.SetActive(false);
+        }
+        
+    }
+}
