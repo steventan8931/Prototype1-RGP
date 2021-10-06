6e0abe882cef9ead4a7de5ef38a670aee10e7930
Steven Tan <tan2001steven@hotmail.com>
2021-10-06 13:07:00 +1300

    New Character Motor + Camera

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,20 +6,10 @@ public class MouseLook : MonoBehaviour
 {
     public Camera m_AttachedCamera;
 
-    public float m_Spin = 0.0f;
-    public float m_Tilt = 0.0f;
-
-    public Vector2 m_TiltExtents = new Vector2(-85.0f, 85.0f);
-
-    public float m_Sensitivty = 2.0f;
-
     public bool m_CursorLocked = true;
 
     public void Awake()
     {
-        m_AttachedCamera.enabled = false;
-        m_Spin = transform.eulerAngles.y;
-        m_Tilt = transform.eulerAngles.x;
         LockCursor();
     }
 
@@ -36,15 +26,5 @@ public class MouseLook : MonoBehaviour
             m_CursorLocked = !m_CursorLocked;
             LockCursor();
         }
-
-        float x = Input.GetAxisRaw("Mouse X");
-        float y = Input.GetAxisRaw("Mouse Y");
-
-        m_Spin += x * m_Sensitivty;
-        m_Tilt -= y * m_Sensitivty;
-
-        m_Tilt = Mathf.Clamp(m_Tilt, m_TiltExtents.x, m_TiltExtents.y);
-
-        transform.localEulerAngles = new Vector3(m_Tilt, m_Spin, 0.0f);
     }
 }
