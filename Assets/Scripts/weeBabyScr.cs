a24e2e1c7ca316fa25e5b6766f432c2ab41ed1e7
peter <hguanqi7@gmail.com>
2021-10-06 20:10:15 +0800

    Baby charge test WIP

--- Assets/Scripts/weeBabyScr.cs
+++ Assets/Scripts/weeBabyScr.cs
@@ -7,16 +7,24 @@ public class weeBabyScr : MonoBehaviour
     public bool istriggered = false;
     public GameObject OldGiant, butcherGiant;
     public Transform loc1, loc2;
+    public GameObject boy;
+    public float moveSpeed = 7f;
     // Start is called before the first frame update
     void Start()
     {
         summonGiants();
+        boy = GameObject.FindGameObjectWithTag("boy");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    Vector3 moveTowards(GameObject target)
+    {
+        return Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed);
+
     }
 
     public void summonGiants()
@@ -28,5 +36,13 @@ public class weeBabyScr : MonoBehaviour
     public void triggerBaby()
     {
         istriggered = true;
+    }
+
+    public void ChargeToPlayer()
+    {
+        Vector3 tempTarget = boy.transform.position;
+        tempTarget.y = gameObject.transform.position.y;
+        transform.LookAt(tempTarget);
+        transform.position = moveTowards(GameObject.FindGameObjectWithTag("boy"));
     }
 }
