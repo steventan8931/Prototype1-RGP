0e868239db7bb06351bfbb6eec9fd00c7e22faf5
peter <hguanqi7@gmail.com>
2021-10-06 11:32:54 +0800

    navmesh fixes and old man scout logic

---
+++ Assets/BalltestScr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class BalltestScr : MonoBehaviour
+{
+    [SerializeField] private Transform movePosTransform;
+    private NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            navMeshAgent.destination = movePosTransform.position;
+        }
+    }
+}
