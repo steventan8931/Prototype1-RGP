5bbaf945fce9b7ef3581fa313d2c29e3623e90c0
Steven Tan <tan2001steven@hotmail.com>
2021-10-18 19:59:32 +1300

    Smash Door Script + Trigger

---
+++ Assets/Scripts/MP1/SmashableDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashableDoor : MonoBehaviour
+{
+    public bool m_IsOpen = false;
+
+    public Transform m_Pivot;
+    public float m_RotationZ = 90.0f;
+
+    public float m_DoorRotateTimer = 0.0f;
+    public float cacheRotation;
+    private void Update()
+    {
+        if (m_IsOpen && m_DoorRotateTimer <= 1.0f)
+        {
+            m_DoorRotateTimer += Time.deltaTime * 0.5f;
+            float ZRotation = Mathf.Lerp(0.0f, m_RotationZ, m_DoorRotateTimer);
+            m_Pivot.rotation = Quaternion.Euler(0.0f, 0.0f, ZRotation);
+            cacheRotation = m_Pivot.rotation.z;
+        }
+
+        if (cacheRotation>= 0.69f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MP1/SmashableTrigger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashableTrigger : MonoBehaviour
+{
+    public GameObject m_Model;
+
+    public bool m_GiantInRange = false;
+    GiantController cahceGiant;
+    HighlightObject m_Highlight;
+
+    public SmashableDoor m_Door;
+    private void Start()
+    {
+        m_Highlight = m_Model.GetComponent<HighlightObject>();
+    }
+
+
+    private void OnTriggerStay(Collider _other)
+    {
+        if (_other.tag == "Giant")
+        {
+            if (_other.GetComponent<CharacterSwapper>().m_IsGiant)
+            {
+                m_GiantInRange = true;
+                cahceGiant = _other.GetComponent<GiantController>();
+                if (cahceGiant.m_Hands.childCount <= 0)
+                {
+                    m_Highlight.m_Change = true;
+                    m_Model.GetComponent<UIPromptControl>().m_CanInteract = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider _other)
+    {
+        if (_other.tag == "Giant")
+        {
+            if (_other.GetComponent<CharacterSwapper>().m_IsGiant)
+            {
+                m_Highlight.m_Change = false;
+                m_Model.GetComponent<UIPromptControl>().m_CanInteract = false;
+                m_GiantInRange = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (m_GiantInRange && cahceGiant.m_Facing)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                cahceGiant.m_Animation.SetBool("Pushing", true);
+                m_Door.m_IsOpen = true;
+            }
+        }
+    }
+}
