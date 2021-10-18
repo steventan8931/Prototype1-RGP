b8c7faf31a894393441cb27bcde61931c5c1be10
Steven Tan <tan2001steven@hotmail.com>
2021-10-18 23:04:47 +1300

    Set up Door in Scene / Moveable Objects WIP in scene

--- Assets/Scripts/MP1/HighlightObject.cs
+++ Assets/Scripts/MP1/HighlightObject.cs
@@ -10,21 +10,21 @@ public class HighlightObject : MonoBehaviour
 
     private void Start()
     {
-        m_Material = GetComponent<Renderer>();
+        //m_Material = GetComponent<Renderer>();
 
-        m_Color = m_Material.material.color;
+        //m_Color = m_Material.material.color;
     }
 
     private void Update()
     {
         if (m_Change)
         {
-            m_Material.material.color = new Vector4(0.5f, 0.5f, 0.0f, 1);
+            //m_Material.material.color = new Vector4(0.5f, 0.5f, 0.0f, 1);
 
         }
         else
         {
-            m_Material.material.color = m_Color;
+            //m_Material.material.color = m_Color;
         }
     }
 }
