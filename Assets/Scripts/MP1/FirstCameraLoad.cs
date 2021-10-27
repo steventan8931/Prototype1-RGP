fa9938f27c3c64bee9ded009cf36bbdb9453776a
Steven Tan <tan2001steven@hotmail.com>
2021-10-27 20:57:39 +1300

    Fix First Camera Pan on Builds

---
+++ Assets/Scripts/MP1/FirstCameraLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCameraLoad : MonoBehaviour
+{
+    public GameObject m_FirstCam;
+
+    public float m_Timer = 0.0f;
+    public float m_EndTimer = 1.0f;
+
+    private void Update()
+    {
+        m_Timer += Time.deltaTime;
+        if (m_Timer > m_EndTimer)
+        {
+            m_FirstCam.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
